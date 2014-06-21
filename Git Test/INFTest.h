fe9ea61843d308931003e9de43958d6f1aa23206
Infinul <infinul@new-host.home>
2014-06-20 21:15:54 -0700

    added test class

---
+++ Git Test/INFTest.h
@@ -0,0 +1,13 @@
+//
+//  INFTest.h
+//  Git Test
+//
+//  Created by Infinul on 6/20/14.
+//  Copyright (c) 2014 Infinul. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface INFTest : NSObject
+
+@end
---
+++ Git Test/INFTest.m
@@ -0,0 +1,13 @@
+//
+//  INFTest.m
+//  Git Test
+//
+//  Created by Infinul on 6/20/14.
+//  Copyright (c) 2014 Infinul. All rights reserved.
+//
+
+#import "INFTest.h"
+
+@implementation INFTest
+
+@end
